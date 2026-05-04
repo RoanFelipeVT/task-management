dc75941524d67b9c511d1ab769413fe8ac2a1349
Roan Felipe Torquato <roanfelipest@gmail.com>
2026-05-04 22:28:15 +0000

    refactor: remover funções de manipulação da esturutra do banco de dados da main

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 from fastapi import FastAPI, HTTPException
-from src.infra.database.config import criar_db, drop_db, engine
-import src.infra.database.models
 from fastapi.middleware.cors import CORSMiddleware 
-from sqlalchemy import inspect
-
-
 
 
 app = FastAPI()
@@ -29,35 +24,7 @@ def read_root():
     return {"message": "Hello, World"}
 
 
-## Endpoint Utilizado Para Teste de Banco de dados
-@app.delete("/database/remove")
-def remove_db():
 
-    inspector = inspect(engine)
 
-    if not inspector.get_table_names():
-        raise HTTPException(
-            status_code=409,
-            detail="No tables to remove"
-        )
 
-    drop_db()
-
-    return {"message": "Database cleared"}
-
-## Endpoint Utilizado Para Teste de Banco de dados
-@app.get("/database/create")
-def create_db():
-
-    inspector = inspect(engine)
-
-    if inspector.get_table_names():
-        raise HTTPException(
-            status_code=409,
-            detail="Tables already exist"
-        )
-
-    criar_db()
-
-    return {"message": "Database initialized"}
 
