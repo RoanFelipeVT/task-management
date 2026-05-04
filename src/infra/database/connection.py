6d10bb9b611fb5fd74c23461548918a7fe521195
Roan Felipe Torquato <roanfelipest@gmail.com>
2026-05-04 22:47:35 +0000

    refactor: isolando Base de connection para prevenir importação circular

--- src/infra/database/connection.py
+++ src/infra/database/connection.py
@@ -1,22 +1,11 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from src.core.config import settings 
-
 
 
 engine = create_engine(settings.DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-Base = declarative_base()
-
-
-def criar_db():
-    Base.metadata.create_all(bind=engine)
-
-def drop_db():
-    Base.metadata.drop_all(bind=engine)
 
 
 def get_db():
